9182055cfa624989b68b55f8c023491007905534
shahab pourazadi <shahabpourazadi@shahabs-MacBook-Pro.local>
2023-06-23 09:43:20 -0700

    Add Project folder

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by shahab pourazadi on 2023-06-23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
